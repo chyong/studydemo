15b70a5e6dd0352768717fbf30af095edb963bed
chenyong <yong.chen@rong-data.com>
2020-12-28 21:04:53 +0800

    最长回文子串

---
+++ src/main/java/com/song/leetcode/LongestSubstring.java
@@ -0,0 +1,18 @@
+package com.song.leetcode;
+
+public class LongestSubstring {
+
+    public static void main(String[] args){
+        System.out.println(longestPalindrome("babad"));
+    }
+
+    public static String longestPalindrome(String s) {
+
+        for (int i = 0; i < s.length(); i++) {
+
+        }
+
+        return s;
+    }
+
+}
