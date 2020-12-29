25fbda73bcffa1eff07bea55422c8c77b8103cd6
cheyong <1871626766@qq.com>
2020-12-29 22:00:29 +0800

    最长回文子串

--- src/main/java/com/song/leetcode/LongestSubstring.java
+++ src/main/java/com/song/leetcode/LongestSubstring.java
@@ -2,17 +2,46 @@ package com.song.leetcode;
 
 public class LongestSubstring {
 
-    public static void main(String[] args){
-        System.out.println(longestPalindrome("babad"));
+    public static void main(String[] args) {
+        System.out.println(longestPalindrome("baabad"));
     }
 
     public static String longestPalindrome(String s) {
-
-        for (int i = 0; i < s.length(); i++) {
-
+        int len = s.length();
+        if (len < 2) {
+            return s;
         }
+        int maxLen = 1;
+        String res = s.substring(0, 1);
+        // 中心位置枚举到 len - 2 即可
+        for (int i = 0; i < len - 1; i++) {
+            String oddStr = centerSpread(s, i, i);
+            String evenStr = centerSpread(s, i, i + 1);
+            String maxLenStr = oddStr.length() > evenStr.length() ? oddStr : evenStr;
+            if (maxLenStr.length() > maxLen) {
+                maxLen = maxLenStr.length();
+                res = maxLenStr;
+            }
+        }
+        return res;
+    }
 
-        return s;
+    private static String centerSpread(String s, int left, int right) {
+        // left = right 的时候，此时回文中心是一个空隙，回文串的长度是奇数
+        // right = left + 1 的时候，此时回文中心是任意一个字符，回文串的长度是偶数
+        int len = s.length();
+        int i = left;
+        int j = right;
+        while (i >= 0 && j < len) {
+            if (s.charAt(i) == s.charAt(j)) {
+                i--;
+                j++;
+            } else {
+                break;
+            }
+        }
+        // 这里要小心，跳出 while 循环时，恰好满足 s.charAt(i) != s.charAt(j)，因此不能取 i，不能取 j
+        return s.substring(i + 1, j);
     }
 
 }
