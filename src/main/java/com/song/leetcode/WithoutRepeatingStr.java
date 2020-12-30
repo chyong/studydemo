a39147c2fdd716830c926a7fa340a94b4769c10c
chenyong <yong.chen@rong-data.com>
2020-12-30 19:06:22 +0800

    最长回文子串1

--- src/main/java/com/song/leetcode/WithoutRepeatingStr.java
+++ src/main/java/com/song/leetcode/WithoutRepeatingStr.java
@@ -1,15 +1,17 @@
 package com.song.leetcode;
 
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
 public class WithoutRepeatingStr {
 
     public static void main(String[] args) {
-        System.out.println(lengthOfLongestSubstring("anagram"));
+        System.out.println(lengthOfLongestSubstring2("anagram"));
     }
 
-    public static int lengthOfLongestSubstring(String s) {
+    public static int lengthOfLongestSubstring1(String s) {
         int n = s.length();
         int res = 0;
         int end = 0, start = 0;
@@ -20,6 +22,24 @@ public class WithoutRepeatingStr {
             }
             map.put(s.charAt(end), end + 1);//map每次更新
             res = Math.max(res, end - start + 1);//结果每次更新
+        }
+        return res;
+    }
+
+    public static int lengthOfLongestSubstring2(String s) {
+        //滑动窗口
+        int n = s.length();
+        int res = 0, start = 0;
+        Set<Character> occ = new HashSet<>();
+        for (int i = 0; i < n; i++) {
+            if(i != 0){
+                occ.remove(s.charAt(i - 1));
+            }
+            while (start < n && !occ.contains(s.charAt(start))){
+                occ.add(s.charAt(start));
+                start++;
+            }
+            res = Math.max(res, start-i);
         }
         return res;
     }
