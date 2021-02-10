0636f62ff739e950faf44e8cd7f4490a6a1aa02b
cheyong <1871626766@qq.com>
2021-02-10 22:53:39 +0800

    19. 删除链表的倒数第 N 个结点

---
+++ src/main/java/com/song/leetcode/RemoveNthFromEnd.java
@@ -0,0 +1,72 @@
+package com.song.leetcode;
+
+/**
+ * 19.删除链表的倒数第 N 个结点
+ */
+public class RemoveNthFromEnd {
+    public static void main(String[] args) {
+        ListNode l1 = new ListNode(3);
+        ListNode l2 = new ListNode(5, l1);
+        ListNode l3 = new ListNode(1, l2);
+        ListNode l4 = new ListNode(9, l3);
+        ListNode l5 = new ListNode(8, l4);
+        ListNode l6 = new ListNode(7, l5);
+        printNode(removeNthFromEnd(l6, 4));
+    }
+
+    public static ListNode removeNthFromEnd(ListNode head, int n) {
+        int index = getLength(head) - n;
+        if (index == -1) return head.next;
+        ListNode cur = head;
+        while (cur.next != null) {
+            index--;
+            if (index == -1) {
+                cur.next = cur.next.next;
+                break;
+            }
+            cur = cur.next;
+        }
+        return head;
+    }
+
+    public static int getLength(ListNode head) {
+        int length = 0;
+        while (head.next != null) {
+            length++;
+            head = head.next;
+        }
+        return length;
+    }
+
+    public static void printNode(ListNode node) {
+        while (node.next != null) {
+            System.out.print(node.val + " ");
+            node = node.next;
+        }
+        System.out.print(node.val);
+    }
+
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+
+        @Override
+        public String toString() {
+            return "ListNode{" +
+                    "val=" + val +
+                    '}';
+        }
+    }
+}
