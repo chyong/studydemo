6004a65ccace37da616a2fa90fcd294322004a80
chenyong <yong.chen@rong-data.com>
2021-03-04 19:16:14 +0800

    122.买卖股票的最佳时机II

---
+++ src/main/java/com/song/leetcode/MaxProfit.java
@@ -0,0 +1,18 @@
+package com.song.leetcode;
+
+/**
+ * 122.买卖股票的最佳时机II
+ */
+public class MaxProfit {
+    public static void main(String[] args) {
+        System.out.println(maxProfit(new int[]{7,1,5,3,6,4}));
+    }
+
+    public static int maxProfit(int[] prices) {
+        for (int i = 0; i < prices.length; i++) {
+
+
+        }
+        return 0;
+    }
+}
