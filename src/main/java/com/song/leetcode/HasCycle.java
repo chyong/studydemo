f85ff2e7fd118d70458b84feb3736188dd54c93e
cheyong <1871626766@qq.com>
2021-03-06 22:47:52 +0800

    141.环形链表

---
+++ src/main/java/com/song/leetcode/HasCycle.java
@@ -0,0 +1,45 @@
+package com.song.leetcode;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * 141.环形链表
+ */
+public class HasCycle {
+    public static void main(String[] args) {
+        ListNode l1 = new ListNode(3);
+        ListNode l2 = new ListNode(5, l1);
+        ListNode l3 = new ListNode(1, l2);
+        ListNode l4 = new ListNode(9, l3);
+        ListNode l5 = new ListNode(8, l4);
+        ListNode l6 = new ListNode(7, l5);
+        System.out.println(hasCycle(l6));
+    }
+
+    public static boolean hasCycle(ListNode head) {
+//        Set<ListNode> seen = new HashSet<>();
+//        while (head != null) {
+//            if (!seen.add(head)) {
+//                return true;
+//            }
+//            head = head.next;
+//        }
+//        return false;
+
+
+        if (head == null || head.next == null) {
+            return false;
+        }
+        ListNode slow = head;
+        ListNode fast = head.next;
+        while (slow != fast) {
+            if (fast == null || fast.next == null) {
+                return false;
+            }
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return true;
+    }
+}
