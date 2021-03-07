f6b04e10370a0edeae7cb07bf74c16570f8a9201
cheyong <1871626766@qq.com>
2021-03-07 22:25:16 +0800

    300.最长递增子序列

---
+++ src/main/java/com/song/leetcode/LengthOfLIS.java
@@ -0,0 +1,29 @@
+package com.song.leetcode;
+
+/**
+ * 300.最长递增子序列
+ */
+public class LengthOfLIS {
+    public static void main(String[] args) {
+        System.out.println(lengthOfLIS(new int[]{10, 9, 2, 5, 3, 7, 101, 18}));
+    }
+
+    public static int lengthOfLIS(int[] nums) {
+        int n = nums.length;
+        if (n == 0) {
+            return 0;
+        }
+        int[] dp = new int[n];
+        int res = 1;
+        for (int i = 0; i < n; i++) {
+            dp[i] = 1;
+            for (int j = 0; j < i; j++) {
+                if (nums[j] < nums[i]) {
+                    dp[i] = Math.max(dp[j] + 1, dp[i]);
+                }
+            }
+            res = Math.max(res, dp[i]);
+        }
+        return res;
+    }
+}
