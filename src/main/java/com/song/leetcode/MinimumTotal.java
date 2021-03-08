274de8c94eab3f50dbadc11629aecbaba98e9074
cheyong <1871626766@qq.com>
2021-03-08 22:52:26 +0800

    120.三角形最小路径和

--- src/main/java/com/song/leetcode/MinimumTotal.java
+++ src/main/java/com/song/leetcode/MinimumTotal.java
@@ -10,20 +10,28 @@ import java.util.List;
 public class MinimumTotal {
     public static void main(String[] args) {
         List<List<Integer>> triangle = new ArrayList<>();
-        triangle.add(Arrays.asList(-1));
-        triangle.add(Arrays.asList(2,3));
-        triangle.add(Arrays.asList(1,-1,-3));
-//        triangle.add(Arrays.asList(4,1,8,3));
+        triangle.add(Arrays.asList(2));
+        triangle.add(Arrays.asList(3, 4));
+        triangle.add(Arrays.asList(6, 5, 7));
+        triangle.add(Arrays.asList(4, 1, 8, 3));
         System.out.println(minimumTotal(triangle));
     }
 
     public static int minimumTotal(List<List<Integer>> triangle) {
         int n = triangle.size();
-        int res = 0;
         int[][] dp = new int[n][n];
-        for (int i = 0; i <n; i++) {
-            for (int j = 0; j <n; j++) {
-
+        dp[0][0] = triangle.get(0).get(0);
+        for (int i = 1; i < n; i++) {
+            dp[i][0] = dp[i - 1][0] + triangle.get(i).get(0);
+            for (int j = 1; j < i; j++) {
+                dp[i][j] = Math.min(dp[i - 1][j - 1], dp[i - 1][j]) + triangle.get(i).get(j);
+            }
+            dp[i][i] = dp[i - 1][i - 1] + triangle.get(i).get(i);
+        }
+        int res = dp[n - 1][0];
+        for (int i = 1; i < n; i++) {
+            if (dp[n - 1][i] < res) {
+                res = dp[n - 1][i];
             }
         }
         return res;
