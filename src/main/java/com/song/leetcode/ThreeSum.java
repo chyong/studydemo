c62b80567e5f765d10b3e359233bef8f80e9c7bf
cheyong <1871626766@qq.com>
2021-04-12 22:56:53 +0800

    修改4

--- src/main/java/com/song/leetcode/ThreeSum.java
+++ src/main/java/com/song/leetcode/ThreeSum.java
@@ -9,48 +9,35 @@ import java.util.List;
  */
 public class ThreeSum {
 
-    public static void main(String[] args){
-        System.out.println(threeSum(new int[]{-1,0,1,2,-1,-4}));
+    public static void main(String[] args) {
+        System.out.println(threeSum(new int[]{-1, 0, 1, 2, -1, -4}));
     }
 
     public static List<List<Integer>> threeSum(int[] nums) {
-        int n = nums.length;
         Arrays.sort(nums);
-        List<List<Integer>> ans = new ArrayList<>();
-        // 枚举 a
-        for (int first = 0; first < n; ++first) {
-            // 需要和上一次枚举的数不相同
-            if (first > 0 && nums[first] == nums[first - 1]) {
-                continue;
-            }
-            // c 对应的指针初始指向数组的最右端
-            int third = n - 1;
-            int target = -nums[first];
-            // 枚举 b
-            for (int second = first + 1; second < n; ++second) {
-                // 需要和上一次枚举的数不相同
-                if (second > first + 1 && nums[second] == nums[second - 1]) {
-                    continue;
-                }
-                // 需要保证 b 的指针在 c 的指针的左侧
-                while (second < third && nums[second] + nums[third] > target) {
-                    --third;
-                }
-                // 如果指针重合，随着 b 后续的增加
-                // 就不会有满足 a+b+c=0 并且 b<c 的 c 了，可以退出循环
-                if (second == third) {
-                    break;
-                }
-                if (nums[second] + nums[third] == target) {
-                    List<Integer> list = new ArrayList<>();
-                    list.add(nums[first]);
-                    list.add(nums[second]);
-                    list.add(nums[third]);
-                    ans.add(list);
+        List<List<Integer>> res = new ArrayList<>();
+        for (int i = 0; i < nums.length; i++) {
+            int target = -nums[i];
+            int l = i + 1;
+            int r = nums.length - 1;
+            if (nums[i] > 0)
+                break;
+            if (i == 0 || nums[i] != nums[i - 1]) {
+                while (l < r) {
+                    if (nums[l] + nums[r] == target) {
+                        res.add(Arrays.asList(nums[i], nums[l], nums[r]));
+                        while (l < r && nums[l] == nums[l + 1]) l++;
+                        while (l < r && nums[r] == nums[r - 1]) r--;
+                        l++;
+                        r--;
+                    } else if (nums[l] + nums[r] < target)
+                        l++;
+                    else
+                        r--;
                 }
             }
         }
-        return ans;
+        return res;
     }
 
 }
